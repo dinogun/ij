5685aca5ac0825d5b4737de693c1ada5b7d6663d
Dinakar Guniguntala <dino@oc1721263563.ibm.com>
2016-02-03 18:16:21 +0530

    Update Dockerfile to use md5sum check option and create the right dir structure Add a Dockerfile for sdk as well. Add a run_java.sh shell script to showcase different IBM Java commandline options

---
+++ ibmjava-8-sdk/run_java.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+/opt/ibm/java/jre/bin/javac -version
+
+# Turn on shared class cache with size 50 MB
+#/opt/ibm/java/jre/bin/java -Xshareclasses:name=sharedCC,cacheDir=/opt/ibm/java/cache,nonfatal,silent -Xscmx50M -version
+# See link for more info on Shared class cache
+# http://www-01.ibm.com/support/knowledgecenter/SSYKE2_8.0.0/com.ibm.java.lnx.80.doc/diag/appendixes/cmdline/Xshareclasses.html?lang=en
+
+
